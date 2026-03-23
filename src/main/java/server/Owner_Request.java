cbbfea9ff6fca5831b6b0ebb00e3dbcf33bb773e
Bernard Joyce <joycebernie833@gmail.com>
2026-03-23 13:29:30 +0000

    added some http methods in request router

---
+++ src/main/java/server/Owner_Request.java
@@ -0,0 +1,17 @@
+package server;
+
+import domain.Owner;
+
+public class Owner_Request {
+    private int id;
+    private Owner owner;
+
+    public int getId() {
+        return id;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+}
--- src/main/java/server/RequestRouter.java
+++ src/main/java/server/RequestRouter.java
@@ -1,23 +1,87 @@
 package server;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import service.*;
 import protocol.*;
 import domain.*;
+
 import java.util.List;
+import java.util.Optional;
 
 public class RequestRouter {
-    private Map<String, RequestHandler> fHandlers = new HashMap<>();
+    private Map<RequestType, RequestHandler> handlers = new HashMap<>();
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private OwnerService ownerService;
+    private CatService catService;
+    private NutritionService nutritionService;
 
     public RequestRouter(OwnerService ownerService, CatService catService, NutritionService nutritionService) {
-        fHandlers.put(RequestType.GET_ALL_OWNERS.name(), req -> {
+        this.catService = catService;
+        this.nutritionService = nutritionService;
+        this.ownerService = ownerService;
+
+        handlers.put(RequestType.GET_ALL_OWNERS, req -> {
             List<Owner> owners = ownerService.listOwners();
             return Response.success("retrieved " + owners.size() + " owners", owners);
         });
+
+        handlers.put(RequestType.GET_OWNER_BY_ID, req -> {
+            Optional<Owner> optionalOwner = ownerService.getOwner(req.getPayload().asInt());
+            if (optionalOwner.isPresent()) {
+                Owner owner = optionalOwner.get();
+                return Response.success("Retrieved: " + owner.getFirstName(), owner);
+            } else {
+                return Response.failure("Owner not found");
+            }
+        });
+
+        handlers.put(RequestType.CREATE_OWNER, req -> {
+            Owner owner = MAPPER.treeToValue(req.getPayload(), Owner.class);
+            ownerService.createOwner(owner);
+            return Response.success("Successfully created owner", owner);
+        });
+
+        handlers.put(RequestType.UPDATE_OWNER, req -> {
+            Owner_Request owner = MAPPER.treeToValue(req.getPayload(), Owner_Request.class);
+            ownerService.updateOwner(owner.getId(), owner.getOwner());
+            return Response.success("Successfully updated owner", owner);
+        });
+
+        handlers.put(RequestType.DELETE_OWNER, req -> {
+            ownerService.deleteOwner(req.getPayload().asInt());
+            return Response.success("Owner deleted successfully", null);
+        });
+
+        handlers.put(RequestType.GET_CAT_BY_ID, (req) -> {
+            Optional<Cat> optionalCat = catService.getCat(req.getPayload().asInt());
+            if (optionalCat.isPresent()) {
+                Cat cat = optionalCat.get();
+                return Response.success("Retrieved: " + cat.getName(), cat);
+            } else {
+                return Response.failure("Cat not found");
+            }
+        });
+
+
+        handlers.put(RequestType.GET_NUTRITION_BY_CAT_ID, (req) -> {
+            Optional<Nutrition> optionalNutrition = nutritionService.getNutrition(req.getPayload().asInt());
+            if (optionalNutrition.isPresent()) {
+                Nutrition owner = optionalNutrition.get();
+                return Response.success("Retrieved Nutrition ", owner);
+            } else {
+                return Response.failure("Nutrition not found");
+            }
+        });
+
+
     }
 
     public Response<?> handleRequest(Request request) {
-        RequestHandler handler = fHandlers.get(request.getType());
+        RequestHandler handler = handlers.get(request.getType());
 
         if (handler == null) {
             return Response.failure("Unknown request type: " + request.getType());
