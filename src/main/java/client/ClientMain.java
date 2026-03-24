d950768cbb8848beb6c239edacf365a7abd6d8f7
MichalS <D00282723@student.dkit.ie>
2026-03-24 06:35:01 +0000

    Feat: create main menu in client main

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -1,8 +1,30 @@
 package client;
 
+import java.util.Scanner;
+
 public class ClientMain {
+    private final Scanner scanner = new Scanner(System.in);
+
     void main() throws Exception {
         try (Client client = new Client("localhost", 9000)) {
+            boolean loopCheck = true;
+            OwnerMenu oMenu = new OwnerMenu(client);
+            while(loopCheck) {
+                System.out.println("\n=== Main Menu ===");
+                System.out.println("1. Cats");
+                System.out.println("2. Owners");
+                System.out.println("3. Nutrients");
+                System.out.println("0. Exit");
+                System.out.print("Choice: ");
+
+                switch(scanner.nextLine().trim()) {
+                    case "1" -> System.out.println("Cats");
+                    case "2" -> oMenu.run();
+                    case "3" -> System.out.println("Nutrients");
+                    case "0" -> loopCheck = false;
+                    default -> System.out.println("Invalid option");
+                }
+            }
 
         }
     }
