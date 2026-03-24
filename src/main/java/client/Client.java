2236fd94b57e53dd66c3628b7df7e55b7b25e79e
MichalS <D00282723@student.dkit.ie>
2026-03-24 05:27:52 +0000

    Feat: send request from Client to Server

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,9 +1,17 @@
 package client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import protocol.RequestType;
+import protocol.Response;
+import protocol.Request;
+
 import java.io.*;
 import java.net.Socket;
 
 public class Client implements AutoCloseable {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
     private final BufferedReader in;
     private final PrintWriter out;
     private final Socket socket;
@@ -12,6 +20,14 @@ public class Client implements AutoCloseable {
         socket = new Socket(host, port);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public Response<JsonNode> send(RequestType type, Object payload) throws IOException {
+        JsonNode payloadNode = MAPPER.valueToTree(payload);
+        Request req = new Request(type.name(), payloadNode);
+        out.println(MAPPER.writeValueAsString(req));
+        String line = in.readLine();
+        return MAPPER.readValue(line, new TypeReference<Response<JsonNode>>() {});
     }
 
     @Override
