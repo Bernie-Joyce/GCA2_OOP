49a9be7c8d5f43f638621a5af58ebda741179106
MichalS <D00282723@student.dkit.ie>
2026-05-06 23:51:23 +0100

    test: request constructor type and payload

--- src/test/java/protocol/RequestTest.java
+++ src/test/java/protocol/RequestTest.java
@@ -2,15 +2,24 @@ package protocol;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RequestTest {
 
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Test
     void defaultConstructor_setsEmptyTypeAndNullPayload() {
         Request req = new Request();
         assertEquals("", req.getType());
         assertNull(req.getPayload());
+    }
+
+    @Test
+    void constructor_setsTypeAndPayload_correctly() {
+        Request req = new Request("GET_ALL_OWNERS", MAPPER.nullNode());
+        assertEquals("GET_ALL_OWNERS", req.getType());
+        assertNotNull(req.getPayload());
     }
 
 }
